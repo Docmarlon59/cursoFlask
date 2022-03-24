807ed97a486275463c041509187295e56a754d09
Docmarlon59 <marlon.axel58@gmail.com>
2022-03-24 13:19:03 -0600

    act 8

--- extends.py
+++ extends.py
@@ -5,10 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-@app.route('/')
+    name='Eduardo'
+    return render_template('index.html', name=name)
+
+@app.route('/user')
 def index():
-    return render_template('index.html')
+    name='Eduardo'
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
